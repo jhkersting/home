8b0c84271195917d610fed14cf32a5b16c271e94
jhkersting <56937882+jhkersting@users.noreply.github.com>
2022-04-04 15:57:31 -0500

    s

--- senate-widget.js
+++ senate-widget.js
@@ -1,17 +1,17 @@
 var nf = d3.format(".1f")
 var dp = d3.timeParse("%m/%d/%y")
-d3.csv("https://data.jhkforecasts.com/2020-senate.csv",data=>{
+d3.csv("https://output.jhkforecasts.com/2020-senate.csv", data => {
     console.log(date)
 
     var sencomp = d3.select("#senate-comp")
-    .append("svg")
-    .attr("viewBox","0 0 300 300")
+        .append("svg")
+        .attr("viewBox", "0 0 300 300")
 
     sencomp.append("line")
-    .attr("x1",12.5)
-    .attr("x2",287.5)
-    .attr("y1",45.5)
-    .attr("y2",45.5)
-    .attr("stroke","black")
+        .attr("x1", 12.5)
+        .attr("x2", 287.5)
+        .attr("y1", 45.5)
+        .attr("y2", 45.5)
+        .attr("stroke", "black")
 
 })
